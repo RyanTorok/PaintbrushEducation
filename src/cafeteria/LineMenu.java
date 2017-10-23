a4a98119fb6c1bd7b9bb2dd35d6eceed5045e2c7
Ryan Torok <11rytorok3900@gmail.com>
2017-10-23 17:50:40 -0500

    added first bit of lunch menu code

---
+++ src/cafeteria/LineMenu.java
@@ -0,0 +1,14 @@
+package cafeteria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineMenu {
+
+    public List<FoodItem> items;
+    public double linePrice;
+
+    public LineMenu() {
+        items = new ArrayList();
+    }
+}
